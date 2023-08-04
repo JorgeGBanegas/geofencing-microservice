55ea2de829ee620e29e09043b3a5203cdbdaf493
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-08-04 19:52:10 -0400

    init commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { urlencoded, json } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const configService = app.get(ConfigService);
+  app.use(morgan('dev')); 
+  app.useGlobalPipes(new ValidationPipe({
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+  app.setGlobalPrefix('api/v1');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));  
+  await app.listen(configService.get('PORT') );
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
